36a7d559053e3c63bc78eec03e637b88636c1fd5
Chad Christopher <devluxv20@gmail.com>
2022-08-21 14:58:15 -0700

    Create custom page layout Fixes #20

---
+++ components/restaurant/Ad.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="ad-container">
+    <h1>Get the best restaurant guide of the worst restaurants</h1>
+    <button class="btn btn-primary btn-large">Buy Now</button>
+  </div>
+</template>
+
+<style scoped>
+  .ad-container {
+    margin: 10rem 0;
+    text-align: center;
+    font-size: 2rem;
+    background-color: rgba(103, 103, 213, 0.3);
+    padding: 3rem;
+  }
+</style>
